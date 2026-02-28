f84ff971246ab17c544eb38070909d94189bd517
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 11:08:55 +0100

    Update launcher.cpp

--- source/launcher.cpp
+++ source/launcher.cpp
@@ -1,1 +1,12 @@
+#ifndef LAUNCHER_HPP
+#define LAUNCHER_HPP
 
+#include <switch.h>
+
+// Alloue la RAM et prépare les services
+void prepareSystemForJava();
+
+// Execute le moteur java avec les paramètres choisis
+void executeJava(const char* version, const char* ram);
+
+#endif
