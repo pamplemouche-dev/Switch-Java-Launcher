b6b3054dc022ef001c157b621e8915737bc694df
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 13:03:16 +0100

    Update launcher.cpp

--- source/launcher.cpp
+++ source/launcher.cpp
@@ -1,13 +1,11 @@
-#include <switch.h>
-#include <stdio.h>
 #include "launcher.hpp"
-
-void prepareSystemForJava() {
-    // Force la Switch en mode haute performance pour les mods
-    appletSetFocusHandlingMode(AppletFocusHandlingMode_NoSuspend);
-}
+#include <stdio.h>
 
 void executeJava(const char* version, const char* ram) {
-    // Ici on simule l'appel au moteur Java
-    printf("Lancement de Minecraft %s avec %s Mo...\n", version, ram);
+    printf("\x1b[1;33m[SYSTEM]\x1b[0m Allocation de %s Mo de RAM...\n", ram);
+    printf("\x1b[1;33m[SYSTEM]\x1b[0m Chargement des bibliothèques LWJGL...\n");
+    
+    // Simulation du lancement de l'OpenJDK
+    printf("\x1b[1;32m[SUCCESS]\x1b[0m Minecraft %s (Mekanism Modded) lancé !\n", version);
+    printf("Redirection vers l'écran de jeu...\n");
 }
