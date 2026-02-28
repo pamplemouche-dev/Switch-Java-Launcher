eab82660c3533903df44c6b7797bcf4dd0537a3c
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 13:05:01 +0100

    Update launcher.hpp

--- include/launcher.hpp
+++ include/launcher.hpp
@@ -1,9 +1,5 @@
 #ifndef LAUNCHER_HPP
 #define LAUNCHER_HPP
-
-#include <switch.h> // <--- DOIT ÊTRE ICI
-
-void prepareSystemForJava();
+#include <switch.h>
 void executeJava(const char* version, const char* ram);
-
 #endif
