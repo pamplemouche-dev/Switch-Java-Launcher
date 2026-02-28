8a5f76d538ff478dcdf4c7a29dd2257f7e264541
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 11:33:16 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,19 @@
 #include <switch.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "launcher.hpp"
-#include "ui.hpp"
 
 int main(int argc, char **argv) {
-    // Init obligatoire
+    // Initialisation de base
     gfxInitDefault();
     consoleInit(NULL);
 
-    printf("\x1b[1;32mLauncher Switch - Mekanism Edition\x1b[0m\n");
+    printf("Launcher Switch Java - Pret!\n");
     printf("Appuyez sur (+) pour quitter.\n");
 
     while(appletMainLoop()) {
         hidScanInput();
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
         if (kDown & KEY_PLUS) break;
-
-        if (kDown & KEY_A) {
-            executeJava("1.16.5", "2500");
-        }
-
         consoleUpdate(NULL);
     }
 
