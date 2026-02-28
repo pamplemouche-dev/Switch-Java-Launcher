9afd57ab60b3aacf5c61ea7f6ab222f6342fa72b
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 14:48:43 +0100

    Update launcher.cpp

--- source/launcher.cpp
+++ source/launcher.cpp
@@ -1,11 +1,7 @@
-#include "launcher.hpp"
+#include "../include/launcher.hpp"
 #include <stdio.h>
 
 void executeJava(const char* version, const char* ram) {
-    printf("\x1b[1;33m[SYSTEM]\x1b[0m Allocation de %s Mo de RAM...\n", ram);
-    printf("\x1b[1;33m[SYSTEM]\x1b[0m Chargement des bibliothèques LWJGL...\n");
-    
-    // Simulation du lancement de l'OpenJDK
-    printf("\x1b[1;32m[SUCCESS]\x1b[0m Minecraft %s (Mekanism Modded) lancé !\n", version);
-    printf("Redirection vers l'écran de jeu...\n");
+    printf("\n\x1b[1;33m[SYSTEM]\x1b[0m RAM: %s Mo\n", ram);
+    printf("\x1b[1;32m[SUCCESS]\x1b[0m Minecraft %s lance !\n", version);
 }
