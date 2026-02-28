1a0570b8232d0c9577fb819621df8732260ea698
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 11:11:32 +0100

    Update ui.cpp

--- source/ui.cpp
+++ source/ui.cpp
@@ -1,1 +1,19 @@
+#include "ui.hpp"
+#include <stdio.h>
 
+void drawHeader() {
+    // On utilise des codes ANSI pour colorer le texte dans la console Switch
+    printf("\x1b[42m\x1b[30m   SWITCH JAVA LAUNCHER - MEKANISM EDITION   \x1b[0m\n\n");
+}
+
+void drawMenu() {
+    printf("\x1b[32m[A]\x1b[0m Lancer Minecraft 1.16.5 + Mekanism\n");
+    printf("\x1b[32m[X]\x1b[0m Lancer Minecraft 1.20.1\n");
+    printf("\x1b[33m[Y]\x1b[0m Nettoyer le cache (.tmp)\n");
+    printf("\x1b[31m[+]\x1b[0m Quitter le launcher\n");
+    printf("\n--------------------------------------------\n");
+}
+
+void showStatus(const char* message) {
+    printf("\x1b[37m[STATUS] %s\x1b[0m\n", message);
+}
