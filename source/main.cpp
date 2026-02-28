9ed4dc9598c586bf26cde39f12b8d56107d4544d
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 13:02:54 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,28 @@
-// On force le chemin vers la bibliothèque Switch
-#include "/opt/devkitpro/libnx/include/switch.h"
+#include <switch.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "launcher.hpp"
+#include "ui.hpp"
+
 int main(int argc, char **argv) {
-    // Initialisation
     gfxInitDefault();
     consoleInit(NULL);
 
-    printf("Test de compilation forcee...\n");
+    printHeader(); // Affiche le titre Mekanism
+    printMenu();   // Affiche les contrôles
 
     while(appletMainLoop()) {
         hidScanInput();
-        if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS) break;
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS) break; // Quitter
+
+        if (kDown & KEY_A) {
+            // Lance Minecraft avec les réglages Mekanism
+            executeJava("1.16.5", "2500"); 
+        }
+
         consoleUpdate(NULL);
     }
 
